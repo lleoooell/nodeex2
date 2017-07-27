1ee2e36400414221321f406e9cdf9bd094aaa5d1
Leo Casagrande <leo@greenberry.io>
2017-07-27 11:42:09 +0200

    commented api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var Eleve = require('./models/eleve.js');
 var app = express();
 
+// j'instance la connection mongo 
 var promise = mongoose.connect('mongodb://localhost:27017/ifa', {
     useMongoClient: true,
 });
@@ -11,22 +12,64 @@ var promise = mongoose.connect('mongodb://localhost:27017/ifa', {
 promise.then(function(db) {
 
     console.log('db.connected');
-
+    // je démarre mon serveur node sur le port 3000
     app.listen(3000, function() {
         console.log('listening on 3000 and database is connected');
     });
 
 });
 
+// j'utilise bodyparser dans toutes mes routes pour parser les res.body en json
 app.use(bodyParser.urlencoded({
-        extended: true
-    }));
+    extended: true
+}));
 
+// je déclare mon dossier qui contient mes vues
+app.set('views', './views');
+// je déclare mon type de moteur de rendu
+app.set('view engine', 'jade');
+
+// je renvoie l'index.html
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html')
 });
 
 
+
+// API : 
+// renvoyer toute la liste des eleves
+app.get('/api/liste', function(req, res) {
+    Eleve.find({}, function(err, collection) {
+        if (err) {
+            console.log(err);
+            return res.send(500);
+        } else {
+            return res.send(collection);
+        }
+    });
+
+});
+
+// renvoie un seul eleve avec son id en param 
+app.get('/api/liste/:id', function(req, res) {
+    console.log(req.params);
+    console.log(req.params.id);
+    Eleve.findOne({
+        "_id": req.params.id
+    }, function(err, monobject) {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        } else {
+            
+            res.send(monobject);
+        }
+    });
+   
+
+});
+
+// gère les requetes post
 app.post('/quotes', function(req, res) {
     console.log(req.body);
     console.log("my name is " + req.body.nom);
@@ -39,38 +82,26 @@ app.post('/quotes', function(req, res) {
     res.send(200);
 
 });
-app.get('/api/liste', function(req, res) {
-     Eleve.find({},function(err, collection){
-        if(err){
-            console.log(err);
-            return res.send(500);
-        }
-        else{
-            return res.send(collection);
-        }
-    });
-    
-});
-app.get('/api/liste/:id', function(req, res) {
+
+// exemple de rendu html / jade
+app.get('/api/liste/jade/:id', function(req, res) {
     console.log(req.params);
     console.log(req.params.id);
-    Eleve.findOne({"_id" : req.params.id},function(err, monobject){
-        if(err){
+    Eleve.findOne({
+        "_id": req.params.id
+    }, function(err, monobject) {
+        if (err) {
             console.log(err);
             return res.send(err);
-        }
-        else{
-            return res.send(monobject);
+        } else {
+            return res.render('profil', {
+                title: 'Hey',
+                nom: monobject.nom,
+                prenom: monobject.prenom
+            });
+           
         }
     });
-    //  Eleve.find({},function(err, collection){
-    //     if(err){
-    //         console.log(err);
-    //         return res.send(500);
-    //     }
-    //     else{
-    //         return res.send(collection);
-    //     }
-    // });
-    
+  
+
 });
